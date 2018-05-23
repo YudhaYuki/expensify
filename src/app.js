48e8c23f3ae1442d066770c082c65d3f152fbb7f
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-23 09:23:29 +0200

    React Router : Install REACT-ROUTER-DOM and First Landing Page Script

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
 
-ReactDOM.render(<p>This is my boiler plate</p>, document.getElementById('app'));
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
+
+

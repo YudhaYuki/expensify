480eddeba2cfe62b3416304acedd3ebc7dc2cfc5
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-23 15:05:55 +0200

    Dispatching Actions - with IF STATEMENT

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,31 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+
+    if (action.type === 'INCREMENT') {
+        return {
+            count: state.count + 1
+        }
+    } else {
+        return state;
+    }
 });
+
+console.log(store.getState());
+
+
+// Actions - That an object that gets sent to the store
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+// I'd like to increment the count
+
+
+// I'd like to reset the count to zero
+
 
 console.log(store.getState());

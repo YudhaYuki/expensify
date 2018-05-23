3aedf316520789c76eacba6efbdeafd13a19528d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-23 22:03:47 +0200

    ES6 Object Destructuring - CHALLENGE

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -35,3 +35,16 @@ if (city && temperature) {
 }
 */
 
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+};
+
+const { name : publisherName = 'Self-Published'} = book.publisher;
+
+console.log(publisherName); // Penguin, Selfpublished
+

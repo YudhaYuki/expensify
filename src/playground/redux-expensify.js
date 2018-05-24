20510ed0ea1b96c4a53d661e70860a20d27cb0e2
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-24 16:24:44 +0200

    Spreading objects - Ex 1. Spreading EDIT_EXPENSE

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -28,6 +28,12 @@ const removeExpense = ({ id } = {}) => ({
 });
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
 // SET_TEXT_FILTER
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
@@ -48,6 +54,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     }
@@ -87,7 +104,7 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 3 
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
-console.log(expenseOne);
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 
 const demoState = {

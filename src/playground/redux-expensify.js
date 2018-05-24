cdd60d941545cae4e0266b598b0115446a4d9e98
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-24 11:23:03 +0200

    Working with Multiple Reducers : STEP 1. Creating default State EMPTY ARRY in this case

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,30 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_ENS_DATE
+
+
+// Creating a store that has two reducers associated with it
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Store creation
+const store = createStore(expensesReducer);
+console.log(store.getState());
+
+
 const demoState = {
     expenses: [{
         id: '1111mjdie',

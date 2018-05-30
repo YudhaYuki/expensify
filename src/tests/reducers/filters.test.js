7c6e2924e62df38398608f29592267a6a5a0e37f
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-30 17:51:19 +0200

    Testing Filters Reducers : SORT_BY_AMOUNT and SORT_BY_DATE

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -10,3 +10,22 @@ test('should setup default filter values', () => {
         endDate: moment().endOf('month')
     });
 });
+
+test('should set sortBy to amount', () => {
+    const state = filtersReducers(undefined, { type: 'SORT_BY_AMOUNT' });
+    expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', () => {
+    const currentState = {
+        text: '',
+        startDate: undefined,
+        endDate: undefined,
+        sortBy: 'amount'
+    };
+
+    const action = {type: 'SORT_BY_DATE'};
+    const state = filtersReducers(currentState, action);
+    expect(state.sortBy).toBe('date');
+});
+

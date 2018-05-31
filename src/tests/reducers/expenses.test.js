b5c26486a62ccba0e6ce554a9dc6422ef163ed11
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-31 09:41:13 +0200

    Testing Expenses Reducer - REMOVE EXPENSE if id exist and not remove if id not exist

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -5,3 +5,25 @@ test('should set default state', () => {
     const state = expensesReducer(undefined, { type: '@@INIT' });
     expect(state).toEqual([]);
 });
+
+test('should remove expense by id', () => {
+    const action = {
+        type : 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense if id not found', () => {
+    const action = {
+        type : 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+
+})
+

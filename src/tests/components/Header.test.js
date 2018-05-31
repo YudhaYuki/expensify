13b448f7d5a5ef6c2659d124bceace6a7b89871d
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-31 11:41:00 +0200

    Enzyme : Install toJSON enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import ReactShalloRenderer from 'react-test-renderer/shallow';
+import toJSON from 'enzyme-to-json'; 
 import Header from '../../components/Header';
 
 
@@ -10,7 +10,10 @@ test('should render Header correctly', () => {
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
 
     const wrapper = shallow(<Header />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+
     // expect(wrapper.find('h1').length).toBe(1);
-    expect(wrapper.find('h1').text()).toBe('Expensify'); // grab the value of h1
+    // expect(wrapper.find('h1').text()).toBe('Expensify'); // grab the value of h1
 
 });

c9b693fa82bf27db192b397fd46fa59c34858436
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-31 12:22:03 +0200

    Enzyme : make toJSON automatically

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json'; 
 import Header from '../../components/Header';
 
-
 test('should render Header correctly', () => {
-    // const renderer = new ReactShalloRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
-
-    const wrapper = shallow(<Header />);
-    expect(toJSON(wrapper)).toMatchSnapshot();
-
-
-    // expect(wrapper.find('h1').length).toBe(1);
-    // expect(wrapper.find('h1').text()).toBe('Expensify'); // grab the value of h1
-
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });

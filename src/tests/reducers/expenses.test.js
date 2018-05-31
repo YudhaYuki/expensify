fdaf21f955138c01d37f47962fbfc62e61aaae6a
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-31 09:50:41 +0200

    Testing Expenses Reducer - ADD_EXPENSE : should add an expense

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -24,6 +24,28 @@ test('should not remove expense if id not found', () => {
 
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
-
 })
+
+// should add an expense
+test('should add an expense', () => {
+    const expense = {
+        id: '109',
+        description: 'Laptop',
+        note: '',
+        createdAt: 20000,
+        amount: 29500
+    };
+
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([...expenses, expense]);
+});
+
+// should edit and expense
+
+// should not edit expense if expense not found
 

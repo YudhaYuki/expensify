c70367f0ac296f41dc47064731b938d7532360f5
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-31 11:22:48 +0200

    Enzyme : Switch the use of React-test-render into Enzyme in Header.test.js file

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactShalloRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import ReactShalloRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShalloRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // const renderer = new ReactShalloRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    const wrapper = shallow(<Header />);
+    // expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('h1').text()).toBe('Expensify'); // grab the value of h1
+
 });

a8228acf94b8b56cda9ece9790b43eb6948244fb
YudhaYuki <YudhaYuki@hotmail.com>
2018-06-02 12:11:56 +0200

    Test Spies: should set new date on date change

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
+
 
 test ('should render ExpenseForm correctly', () => {
     const wrapper = shallow(<ExpenseForm />);
@@ -78,4 +80,11 @@ test('should call onSubmit prop for valid for submission', () => {
         createdAt: expenses[0].createdAt
 
     });
+});
+
+test('should set new date on date change', () => {
+    const now = moment();
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('SingleDatePicker').prop('onDateChange')(now);
+    expect(wrapper.state('createdAt')).toEqual(now);
 });

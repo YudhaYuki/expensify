c4a44a219463420875d5418a41aec55517710059
YudhaYuki <YudhaYuki@hotmail.com>
2018-06-03 08:46:21 +0200

    Testing EditExpensePage: Setup mapDispatchToProps editExpense and removeExpense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -17,11 +17,11 @@ import { editExpense, removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.dispatch(editExpense(this.props.expense.id, expense));
+        this.props.editExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
     onRemove = () => {
-        this.props.dispatch(removeExpense({ id: this.props.expense.id }));
+        this.props.removeExpense({ id: this.props.expense.id });
         this.props.history.push('/');
     };
     render() {
@@ -37,10 +37,13 @@ export class EditExpensePage extends React.Component {
     }
 }
 
-const mapStateToProps = (state, props) => {
-    return {
-        expense: state.expenses.find((expense) => expense.id === props.match.params.id)
-    };
-};
+const mapStateToProps = (state, props) => ({
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+});
+    
+const mapDispatchToProps = (dispatch) => ({
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (id, expense) => dispatch(removeExpense(id, expense))
+});
 
-export default connect(mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
